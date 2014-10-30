e093d2b7b31f9e2e895fa01aee7db194dfa25280
Joe Dursun <joedursun@gmail.com>
2014-10-30 16:49:13 -0400

    Initial setup

---
+++ user.el
@@ -0,0 +1,83 @@
+;; Rebind keys
+;;(when (eq system-type 'darwin)
+;;  (setq mac-command-modifier 'meta)
+;;  (setq mac-option-modifier 'super))
+
+;; Stu's init.el
+;; https://gist.github.com/stuarth/98e5d6ad473d294e4037
+
+(setq initial-frame-alist '((top . 0) (left . 55) (width . 200) (height . 60)))
+
+(require 'linum)
+(line-number-mode 1)
+(column-number-mode 1)
+(global-linum-mode 1)
+
+(auto-compression-mode 1)
+(add-hook 'prog-mode-hook #'paredit-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+
+(when (null package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(better-defaults
+                      cljsbuild-mode
+                      clojure-mode
+                      company
+                      dash
+                      elisp-slime-nav
+                      find-file-in-project
+                      idle-highlight-mode
+                      ido-ubiquitous
+                      magit
+                      paredit
+                      projectile
+                      ruby-end
+                      slim-mode
+                      smart-tab
+                      starter-kit-lisp))
+
+(when (not (file-exists-p "~/.emacs.d/elpa"))
+  (package-refresh-contents))
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(require 'ruby-end)
+(require 'better-defaults)
+(require 'smart-tab)
+(require 'company)
+
+(custom-set-variables
+ '(company-idle-delay nil)
+ '(smart-tab-completion-functions-alist
+   '((emacs-lisp-mode . company-complete)
+     (text-mode . dabbrev-completion)
+     (clojure-mode . company-complete)
+     (cider-repl-mode . company-complete))))
+
+(add-hook 'write-file-hooks 'delete-trailing-whitespace)
+(add-hook 'after-init-hook 'global-company-mode)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'load-path "~/.emacs.d/themes")
+
+(load-theme 'zenburn t)
+(remove-hook 'text-mode-hook 'turn-on-flyspell)
