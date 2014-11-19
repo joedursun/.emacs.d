16a89d6ac7a9c02041f948b16128663eba8d33a8
Joe Dursun <joedursun@gmail.com>
2014-11-18 23:13:38 -0500

    Update ido-ubiquitous

--- elpa/ido-ubiquitous-20141118.1234/ido-ubiquitous-autoloads.el
+++ elpa/ido-ubiquitous-20141118.1234/ido-ubiquitous-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "ido-ubiquitous" "ido-ubiquitous.el" (21593
-;;;;;;  42008 0 0))
+;;;### (autoloads nil "ido-ubiquitous" "ido-ubiquitous.el" (21612
+;;;;;;  4091 0 0))
 ;;; Generated autoloads from ido-ubiquitous.el
 
 (define-obsolete-variable-alias 'ido-ubiquitous 'ido-ubiquitous-mode "0.8")
