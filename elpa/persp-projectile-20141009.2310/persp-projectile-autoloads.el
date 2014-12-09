52cd022d7d5293cef88e889c02e6a30e68b08a83
Joe Dursun <joedursun@gmail.com>
2014-12-08 23:16:46 -0500

    Update installed packages

--- elpa/persp-projectile-20141009.2310/persp-projectile-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; persp-projectile-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("persp-projectile.el") (21594 25501 189044
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; persp-projectile-autoloads.el ends here
